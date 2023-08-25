5fb29e1db34a76ca73f14a36685d2e1710e74103
Mykhailo Kuryk <133024859+mykhailoKrlKrk@users.noreply.github.com>
2023-08-25 18:20:59 +0300

    Revert "add controllers and corresponding changes"

--- src/main/java/mate/academy/book/store/repository/SpecificationProvider.java
+++
@@ -1,9 +0,0 @@
-package mate.academy.book.store.repository;
-
-import org.springframework.data.jpa.domain.Specification;
-
-public interface SpecificationProvider<T> {
-    String getKey();
-
-    Specification<T> getSpecification(String[] params);
-}
